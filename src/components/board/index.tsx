0f1ef68080e3a3db82f71202bcde45497e928019
Brspontes <brian.robert16@hotmail.com>
2021-03-14 19:48:15 -0300

    finishing drag and drop

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { loadLists } from '../../services/api'
 import List from '../list'
 import { Container } from './styles'
+import BoardContext from '../contexts/context'
+import produce from 'immer'
 
-const lists = loadLists()
+const data = loadLists()
 
 export default function Board() {
+
+  const [lists, setLists] = useState(data)
+
+  function move(fromList: number, toList: number, from: number, to: number) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from]
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      {lists.map(list => <List key={list.title} data={list} />)}
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
+      </Container>
+    </BoardContext.Provider>
   )
 }
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import { useDrag } from 'react-dnd'
+import React, { useRef, useContext } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
 import { Container, Label } from './styles'
+import BoardContext from '../contexts/context'
 
-export default function Card({ data }: any) {
+export default function Card({ data, index, listIndex }: any) {
+
+  const ref = useRef()
 
   const [{ isDragging }, dragRef] = useDrag({
     type: 'CARD',
+    item: { index, listIndex },
     collect: monitor => ({
-      isDragging: monitor.isDragging
+      isDragging: monitor.isDragging(),
     })
   })
 
+  const { move } = useContext(BoardContext)
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover: (item, monitor) => {
+      const draggedIndex = item.index
+      const targetIndex = index
+
+      const draggedListIndex = item.listIndex
+      const targetListIndex  = listIndex
+
+      if(draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+        return
+      }
+
+      //returns size component
+      const targetSize = ref.current.getBoundingClientRect()
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2
+
+      const draggedOffSet = monitor.getClientOffset();
+      const draggedTop = draggedOffSet.y - targetSize.top
+
+      if(draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+
+      if(draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return;
+      }
+
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex)
+
+      item.index = targetIndex
+      item.listIndex = targetListIndex
+    }
+  })
+
+  dragRef(dropRef(ref))
+
   return (
     // @ts-ignore
-    <Container ref={dragRef} isDragging={isDragging}>
+    <Container ref={ref} isDragging={isDragging}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
